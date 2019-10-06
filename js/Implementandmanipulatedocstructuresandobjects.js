b354c9db58de77de06ba8dddab2ef9042519bc95
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-10-06 18:28:06 +0100

    New pages and js

--- js/Implementandmanipulatedocstructuresandobjects.js
+++ js/Implementandmanipulatedocstructuresandobjects.js
@@ -1,6 +1,3 @@
 //@ts-check
 'use strict';
 
-var inner = document.getElementById(innerDiv);
-var element = inner.insertBefore(document.createElement("article"), inner.firstChild);
-element.innerText = "My new <article> element";
--- js/app.js
+++ js/app.js
@@ -9,3 +9,8 @@ let todaysDate = new Date();
 let formatDate = todaysDate.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+//insert a new tag dynamically
+var inner = document.getElementById("innerDiv");
+var element = inner.insertBefore(document.createElement("article"), inner.firstChild);
+element.innerText = "My new <article> element";

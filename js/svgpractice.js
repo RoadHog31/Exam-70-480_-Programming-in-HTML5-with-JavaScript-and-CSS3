120f14a825f856e7e8933d1bec848e0dc90d45ca
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-10-22 20:33:03 +0100

    svg stuff

--- js/svgpractice.js
+++ js/svgpractice.js
@@ -1,3 +1,22 @@
 //@ts-check
 'use strict';
 
+//onload event encapsulates your code and renders when page loads.
+/*window.onload = function () {
+
+    var drawingSurface = document.getElementById("circle");
+
+
+}*/
+
+function Red(evt) {
+
+    var circle = evt.target;
+    circle.setAttribute("style", "fill: red");
+}
+
+function Green(evt) {
+    
+    var circle = evt.target;
+    circle.setAttribute("style", "fill: green");
+}

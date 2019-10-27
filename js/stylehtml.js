17d50b5858099d7417596be253a3a2f3d0b8cd45
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-10-27 19:55:24 +0000

    js

---
+++ js/stylehtml.js
@@ -0,0 +1,16 @@
+"use strict";
+//@ts-check
+
+window.onload = function (){
+
+    var top = document.getElementById("topText");
+    var left = document.getElementById("leftText");
+    var pos = document.getElementById("positioning");
+    document.getElementById("btnPosition").onclick = function() {
+
+        var img = document.getElementById("arrow2");
+        img.style.position = pos.value;
+        img.style.left = left.value + "px";
+        img.style.top = top.value + "px";
+    }
+}

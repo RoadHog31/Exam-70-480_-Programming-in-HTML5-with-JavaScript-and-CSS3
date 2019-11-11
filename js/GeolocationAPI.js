151194ca966c59d14e50a07d43543ef6a5122d14
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-11-11 20:08:14 +0000

    m

--- js/GeolocationAPI.js
+++ js/GeolocationAPI.js
@@ -28,15 +28,14 @@ function errorPosition(err) {
     document.getElementById("geoResults").appendChild(sp);
 } */
 
-   //LISTING 1-6  Using the Geolocation API to monitor position
-   
+   //LISTING 1-6  Using the Geolocation API to monitor position   
     var watcher;
     var geoLocator;
     
     window.onload = function () {    
     geoLocator = window.navigator.geolocation;    
     var posOptions = {enableHighAccuracy: true,timeout: 45000}; 
-       
+
     watcher = geoLocator.watchPosition(successPosition, errorPosition, posOptions);
 }
 
@@ -44,7 +43,7 @@ function successPosition(pos)
 {    
     var sp = document.createElement("p");    
     sp.innerText = "Latitude: " + pos.coords.latitude + 
-    " Longitude: "     + pos.coords.longitude;    
+    " Longitude: " + pos.coords.longitude;    
     document.getElementById("geoResults").appendChild(sp);    
     geoLocator.clearWatch(watcher);
 

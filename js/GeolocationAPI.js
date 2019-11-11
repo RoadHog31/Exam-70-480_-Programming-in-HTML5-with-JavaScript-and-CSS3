a498062ff7c870b237491a9afeea7ca942f57d62
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-11-11 19:50:19 +0000

    a

--- js/GeolocationAPI.js
+++ js/GeolocationAPI.js
@@ -11,13 +11,51 @@ window.onload = function (){
     geolocator.getCurrentPosition(successPosition, errorPosition, posOptions);
 
 }
+
 function successPosition(pos){
 
-    alert(pos);
+    var sp = document.createElement("p");
+    sp.innerText = "Latitude: " + pos.coords.latitude + 
+                    " Longitude: " + pos.coords.longitude +
+                    " Time Stamp: " + pos.timestamp;
+                    document.getElementById("geoResults").appendChild(sp);
 }
+
 function errorPosition(err) {
     
-    alert(err);
+    var sp = document.createElement("p");
+    sp.innerText = "error: " + err.message; + " code: " + err.code;
+    document.getElementById("geoResults").appendChild(sp);
 }
+
+   /* //LISTING 1-6  Using the Geolocation API to monitor position
+   
+    var watcher;
+    var geoLocator;
+    
+    window.onload = function () {    
+    geoLocator = window.navigator.geolocation;    
+    var posOptions = {enableHighAccuracy: true,timeout: 45000};    
+    watcher = geoLocator.watchPosition(successPosition, errorPosition, posOptions);
+}
+
+function successPosition(pos) 
+{    
+    var sp = document.createElement("p");    
+    sp.innerText = "Latitude: " + pos.coords.latitude + 
+    " Longitude: "     + pos.coords.longitude;    
+    document.getElementById("geoResults").appendChild(sp);    
+    geoLocator.clearWatch(watcher);
+
+}
+
+function errorPosition(err) 
+{    
+
+    var sp = document.createElement("p");    
+    sp.innerText = "error: " + err.message; + " code: " + err.code;    
+    document.getElementById("geoResults").appendChild(sp);
+
+} */
 
 

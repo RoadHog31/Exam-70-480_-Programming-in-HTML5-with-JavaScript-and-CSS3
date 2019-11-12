d266d11aeea2300350667a445af15553a4ca27eb
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-11-12 20:19:54 +0000

    varablescope

--- js/variablelifetimeandscope.js
+++ js/variablelifetimeandscope.js
@@ -1,16 +1,29 @@
+//global to the entire page. 
 var globalVar = "global";
 
+//window.onload event handler.
 window.onload = function(){
 
+    //local to the onload event handler. 
+    //Inside the onload event handler, the code has access to the globalVar variable.
     var localVar = "local";
+
     document.getElementById("Div1").onclick = function ()
     {
-            var insideDivClick;
-            //Do some logic...
-            
+            var insideDivClick = "insideDiv1";
+            //Do some logic...  
+            alert(globalVar);
+            alert(localVar);
+            alert(insideDivClick);        
             
     };
     document.getElementById("Div2").onclick = function(){
+
+        var insideDivClick = "insideDiv2";
+            //Do some logic...  
+            alert(globalVar);
+            alert(localVar);
+            alert(insideDivClick); 
     };
     document.getElementById("Div3").onclick = function(){
     };

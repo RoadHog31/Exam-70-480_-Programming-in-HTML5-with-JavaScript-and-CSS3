edb03ae2c1f38034ba7a97cfd33cfa1bce1b1557
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-11-12 20:07:34 +0000

    y

---
+++ js/variablelifetimeandscope.js
@@ -0,0 +1,25 @@
+var globalVar = "global";
+
+window.onload = function(){
+
+    var localVar = "local";
+    document.getElementById("Div1").onclick = function ()
+    {
+            var insideDivClick;
+            //Do some logic...
+            
+            
+    };
+    document.getElementById("Div2").onclick = function(){
+    };
+    document.getElementById("Div3").onclick = function(){
+    };
+    function AFunction(){
+
+        var x;
+    }
+    function BFunctionWithParam(p){
+
+    }
+
+}
